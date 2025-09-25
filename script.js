bb27c6837ca01abca6a85a244ddc7a8e1f32bdf6
EmersonMafra51 <emersonmafra51@gmail.com>
2025-09-25 10:34:37 -0300

    Jogo rodando no index

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-const button = document.getElementById('playButton');
+
+  const button = document.getElementById('playButton');
   const container = document.getElementById('gameContainer');
 
   button.addEventListener('click', () => {
-    // Cria o iframe
+    // Cria o iframe do embed do itch.io
     const iframe = document.createElement('iframe');
-    iframe.src = "https://itch.io/embed/2646330";
+    iframe.src = "https://itch.io/embed/2646330"; // link do embed, não da página do jogo
+    iframe.frameBorder = "0";
     iframe.allowFullscreen = true;
+
     container.appendChild(iframe);
 
     // Mostra o container e esconde o botão
     container.style.display = 'block';
     button.style.display = 'none';
   });
+
